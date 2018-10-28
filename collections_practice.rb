11a4a844027e2bcc18dc52d7501cf238c7bf3298
Nicole D <nnardella@gmail.com>
2018-10-28 17:33:39 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,7 +47,6 @@ end
 def add_s(array)
   array.each_with_index.collect do |element, index|
     "#{element}s"
-    array.delete_at(1)
-    array.insert(1, 'feet') 
+ array.index[1] = "feets"
   end
 end
